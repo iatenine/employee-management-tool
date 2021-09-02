f0a16033bdeef88772eac9a8d5132d15353f5e70
Jack Linhart <iatenine@gmail.com>
2021-09-02 01:50:47 -0600

    Department and Roles' C+R functions working

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const inquirer = require("inquirer");
 const DataManager = require("./src/DataManager");
 
-const choiceList = ["VIEW DEPARTMENTS", "ADD DEPARTMENT", "EXIT"];
+const choiceList = [
+  "VIEW DEPARTMENTS", //0
+  "VIEW ROLES",
+  "VIEW EMPLOYEES",
+  "ADD DEPARTMENT",
+  "ADD ROLE",
+  "ADD EMPLOYEE",
+  "UPDATE EMPLOYEE ROLE",
+  "EXIT",
+];
 const dataManager = new DataManager();
 
 const questions = [
@@ -17,17 +26,53 @@ const deptAddQuestions = [
   {
     type: "string",
     name: "name",
-    message: "What is the name of the new department?",
+    message: "Enter new department's name",
   },
 ];
 
-async function specifyName() {
+const roleAddQuestions = [
+  {
+    type: "string",
+    name: "title",
+    message: "Enter new role's title",
+  },
+  {
+    type: "number",
+    name: "salary",
+    message: "Enter role's salary",
+  },
+  {
+    type: "number",
+    name: "department_id",
+    message: "Enter ID of department Role belongs to",
+  },
+];
+
+async function setDeparmentData() {
   try {
     const answer = await inquirer.prompt(deptAddQuestions);
     dataManager.addDepartment(answer.name);
   } catch (err) {
     console.error(err);
   }
+}
+
+async function setRoleData() {
+  try {
+    const answer = await inquirer.prompt(roleAddQuestions);
+    dataManager.addRole(answer.title, answer.salary, answer.department_id);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function setEmployeeData() {
+  // try {
+  //   const answer = await inquirer.prompt(deptAddQuestions);
+  //   dataManager.addDepartment(answer.name);
+  // } catch (err) {
+  //   console.error(err);
+  // }
 }
 
 async function promptQuestions() {
@@ -39,7 +84,19 @@ async function promptQuestions() {
         dataManager.getDepartments();
         break;
       case choiceList[1]:
-        await specifyName();
+        dataManager.getRoles();
+        break;
+      case choiceList[2]:
+        dataManager.getEmployees();
+        break;
+      case choiceList[3]:
+        await setDeparmentData();
+        break;
+      case choiceList[4]:
+        await setRoleData();
+        break;
+      case choiceList[5]:
+        await setEmployeeData();
         break;
       default:
         console.log("Goodbye!");
@@ -48,7 +105,6 @@ async function promptQuestions() {
     promptQuestions();
   } catch (err) {
     if (err) throw new Error(err);
-    console.log("why are we here?");
   }
 }
 
--- src/DataManager.js
+++ src/DataManager.js
@@ -1,3 +1,4 @@
+const ConsoleTable = require("console.table");
 const connection = require("../config/connection");
 
 class DataManager {
@@ -6,13 +7,38 @@ class DataManager {
   getDepartments() {
     connection.execute("SELECT * FROM departments", (err, result) => {
       if (err) console.error(err);
-      console.log("Result: ", result);
+      console.table(result);
+    });
+  }
+
+  getRoles() {
+    connection.execute("SELECT * FROM roles", (err, result) => {
+      if (err) console.error(err);
+      console.table(result);
+    });
+  }
+
+  getEmployees() {
+    connection.execute("SELECT * FROM employees", (err, result) => {
+      if (err) console.error(err);
+      console.table(result);
     });
   }
 
   addDepartment(name) {
     connection.execute(
       `INSERT INTO departments (name) values ('${name}')`,
+      (err, result) => {
+        if (err) throw new Error(err);
+        console.log("Department Added!");
+        console.log(`${result}`);
+      }
+    );
+  }
+
+  addRole(title, salary, department_id) {
+    connection.execute(
+      `INSERT INTO departments (title, salary, department_id) values ('${tille}', '${salary}', '${department_id}')`,
       (err, result) => {
         if (err) throw new Error(err);
         console.log("Department Added!");
